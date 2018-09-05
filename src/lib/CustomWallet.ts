3c8b4ea5995b711ae396099d960fc8deeba00fcd
Kevin West <keevon@gmail.com>
2018-09-04 22:51:55 -0700

    Add types for web3 transaction data, add network selection

--- src/lib/CustomWallet.ts
+++ src/lib/CustomWallet.ts
@@ -1,11 +1,14 @@
+import { Tx } from "web3/eth/types";
+
 interface IData {
   name: string;
   iconURL?: string;
 }
 
 interface IFunctions {
-  signTransaction?: (txData: any) => Promise<any>;
+  approveTransaction?: (txData: Tx) => Promise<boolean>;
   getAccounts?: () => Promise<string[]>;
+  signTransaction?: (txData: Tx) => Promise<string>;
 }
 
 /**
@@ -31,13 +34,13 @@ export default class CustomWallet {
     return this.data.iconURL || "";
   }
 
-  public signTransaction(txData: any): Promise<any> {
-    if (!this.functions.signTransaction) {
+  public approveTransaction(txData: Tx): Promise<boolean> {
+    if (!this.functions.approveTransaction) {
       throw new Error(
-        "If requesting a custom wallet, you must define a signTransaction handler"
+        "If requesting a custom wallet, you must define a approveTransaction handler"
       );
     }
-    return this.functions.signTransaction(txData);
+    return this.functions.approveTransaction(txData);
   }
 
   public getAccounts(): Promise<string[]> {
@@ -49,13 +52,28 @@ export default class CustomWallet {
     return this.functions.getAccounts();
   }
 
-  public handleSignTransaction(signTransaction: (txData: any) => Promise<any>) {
-    this.functions.signTransaction = signTransaction;
+  public signTransaction(txData: Tx): Promise<string> {
+    console.log(txData);
+    if (!this.functions.signTransaction) {
+      throw new Error(
+        "If requesting a custom wallet, you must define a signTransaction handler"
+      );
+    }
+    return this.functions.signTransaction(txData);
+  }
+
+  public handleApproveTransaction(approveTransaction: (txData: Tx) => Promise<boolean>) {
+    this.functions.approveTransaction = approveTransaction;
     return this;
   }
 
   public handleGetAccounts(getAccounts: () => Promise<string[]>) {
     this.functions.getAccounts = getAccounts;
+    return this;
+  }
+
+  public handleSignTransaction(signTransaction: (txData: Tx) => Promise<string>) {
+    this.functions.signTransaction = signTransaction;
     return this;
   }
 }
--- src/lib/HydroSwap.ts
+++ src/lib/HydroSwap.ts
@@ -1,15 +1,16 @@
 import Penpal from "penpal";
+import { Tx } from "web3/eth/types";
 import CustomWallet from "./CustomWallet";
 
+type Network = "main" | "ropsten" | "local";
 type Wallet = "metamask" | "ledger" | "custom";
 
 /**
  *
  */
 export default class HydroSwap {
-  private static BASE_URL: string = "https://widget.hydroprotocol.io";
-
   private id: string;
+  private network: Network;
 
   private defaultAmount?: number;
   private defaultWallet?: Wallet;
@@ -17,12 +18,13 @@ export default class HydroSwap {
 
   private customWallet?: CustomWallet;
 
-  constructor(id: string) {
+  constructor(id: string, network?: Network) {
     this.id = id;
+    this.network = network || "main";
   }
 
   public attach(container: HTMLElement) {
-    let url = new URL(this.id, HydroSwap.BASE_URL);
+    let url = new URL(this.id, this.getBaseURL());
     let params = new URLSearchParams();
     if (this.defaultAmount) {
       params.append("amount", this.defaultAmount.toFixed(2));
@@ -49,10 +51,12 @@ export default class HydroSwap {
         url: url.href,
         appendTo: rounded,
         methods: {
-          signTransaction: (txData: any): Promise<any> =>
-            this.getCustomWallet().signTransaction(txData),
+          approveTransaction: (txData: Tx): Promise<boolean> =>
+            this.getCustomWallet().approveTransaction(txData),
           getAccounts: (): Promise<string[]> =>
             this.getCustomWallet().getAccounts(),
+          signTransaction: (txData: Tx): Promise<string> =>
+            this.getCustomWallet().signTransaction(txData),
           getIconURL: (): string => this.getCustomWallet().iconURL,
           getName: (): string => this.getCustomWallet().name
         }
@@ -92,5 +96,16 @@ export default class HydroSwap {
       throw new Error("Custom wallet handler must be defined.");
     }
     return this.customWallet;
+  }
+
+  private getBaseURL(): string {
+    switch (this.network) {
+      case "main":
+        return "https://widget.hydroprotocol.io";
+      case "ropsten":
+        return "https://widget-ropsten.hydroprotocol.io";
+      case "local":
+        return "https://localhost:4000";
+    }
   }
 }
